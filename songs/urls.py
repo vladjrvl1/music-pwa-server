86189470466a73bd3b50ad99a1d066c0a716a8bc
Vladyslav Zhuravel <vladislav.zhuravel@raccoongang.com>
2024-06-28 19:07:17 +0300

    feat: Add range header

--- songs/urls.py
+++ songs/urls.py
@@ -3,7 +3,9 @@ from django.views.decorators.csrf import csrf_exempt
 from graphene_file_upload.django import FileUploadGraphQLView
 
 from .schema import schema
+from .views import stream_audio
 
 urlpatterns = [
     path('graphql/', csrf_exempt(FileUploadGraphQLView.as_view(graphiql=True, schema=schema))),
+    path('stream/<int:song_id>', stream_audio, name='stream_audio'),
 ]
